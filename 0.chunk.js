c9d9a1a5d6b11198b50fa4a66354aa5e75e8711f
The Buildbot <buildbot@angular2buch.de>
2016-11-30 21:26:51 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-buch/book-monkey2/commit/12f4938f5a10d52ada435b1f7c23e8899a4f7d97
    Travis build: https://travis-ci.org/angular-buch/book-monkey2/builds/180209925

--- 0.chunk.js
+++ 0.chunk.js
@@ -5426,7 +5426,7 @@ var BookFormComponent = (function () {
             isbn: [this.book.isbn, [
                     __WEBPACK_IMPORTED_MODULE_2__angular_forms__["Validators"].required,
                     this.bv.isbnFormat
-                ], this.isUpdatingBook ? null : this.bv.isbnExists.bind(this)],
+                ], this.isUpdatingBook ? null : this.bv.isbnExists(this.bs)],
             description: [this.book.description],
             authors: this.buildAuthorsArray(),
             thumbnails: this.buildThumbnailsArray(),
@@ -5498,7 +5498,6 @@ var BookFormComponent = (function () {
 
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__shared_book_store_service__ = __webpack_require__(342);
 /* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return BookValidatorsService; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -5510,10 +5509,8 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 
-
 var BookValidatorsService = (function () {
-    function BookValidatorsService(bs) {
-        this.bs = bs;
+    function BookValidatorsService() {
     }
     BookValidatorsService.prototype.isbnFormat = function (control) {
         if (!control.value) {
@@ -5533,18 +5530,19 @@ var BookValidatorsService = (function () {
             atLeastOneAuthor: { valid: false }
         };
     };
-    BookValidatorsService.prototype.isbnExists = function (control) {
-        return this.bs.check(control.value)
-            .map(function (exists) { return (exists === false) ? null : {
-            isbnExists: { valid: false }
-        }; });
+    BookValidatorsService.prototype.isbnExists = function (bs) {
+        return function (control) {
+            return bs.check(control.value)
+                .map(function (exists) { return (exists === false) ? null : {
+                isbnExists: { valid: false }
+            }; });
+        };
     };
     BookValidatorsService = __decorate([
         __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(), 
-        __metadata('design:paramtypes', [(typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__shared_book_store_service__["a" /* BookStoreService */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_1__shared_book_store_service__["a" /* BookStoreService */]) === 'function' && _a) || Object])
+        __metadata('design:paramtypes', [])
     ], BookValidatorsService);
     return BookValidatorsService;
-    var _a;
 }());
 
 
